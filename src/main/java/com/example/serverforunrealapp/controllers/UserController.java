49701e636ece64aa5d8d86b103e6ccc28c0340b1
Egor Ralovich <vonkad002@gmail.com>
2022-11-16 11:52:03 +0300

    Update UserController.java

--- src/main/java/com/example/serverforunrealapp/controllers/UserController.java
+++ src/main/java/com/example/serverforunrealapp/controllers/UserController.java
@@ -1,7 +1,7 @@
 package com.example.serverforunrealapp.controllers;
 
-import com.example.serverforunrealapp.models.UserModel;
 import com.example.serverforunrealapp.repos.UserRepo;
+import com.example.serverforunrealapp.servises.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,21 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
     private final UserRepo userRepo;
+    private final UserService userService;
 
-    public UserController(UserRepo userRepo) {
+    public UserController(UserRepo userRepo, UserService userService) {
         this.userRepo = userRepo;
+        this.userService = userService;
     }
 
     @PostMapping("/register")
-    public void register(@RequestParam String login,
-                         @RequestParam String password,
-                         @RequestParam String name,
-                         @RequestParam String lastName){
-        UserModel userModel = new UserModel();
-        userModel.setName(name);
-        userModel.setLastName(lastName);
-        userModel.setPassword(password);
-        userModel.setLogin(login);
-        userRepo.save(userModel);
+    public boolean register(@RequestParam String login,
+                            @RequestParam String password,
+                            @RequestParam String name,
+                            @RequestParam String lastName){
+      return userService.register(login, password, name, lastName);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String login, @RequestParam String password) {
+return userService.login(login, password);
     }
 }
