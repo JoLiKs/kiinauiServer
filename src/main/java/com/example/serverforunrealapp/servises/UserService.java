90d448afa239eb2905a92c65d8a728b6d1057a1f
Egor Ralovich <vonkad002@gmail.com>
2022-11-16 11:53:18 +0300

    Update UserService.java

--- src/main/java/com/example/serverforunrealapp/servises/UserService.java
+++ src/main/java/com/example/serverforunrealapp/servises/UserService.java
@@ -1,8 +1,9 @@
-package com.example.serverforunrealapp.servises;
-
 import com.example.serverforunrealapp.models.UserModel;
 import com.example.serverforunrealapp.repos.UserRepo;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -10,6 +11,23 @@ public class UserService {
 
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
+    }
+
+    public boolean register(String login,
+                         String password,
+                        String name,
+                        String lastName){
+        List<String> logins = userRepo.findLogins();
+        if (logins.contains(logins)) {
+            return false;
+        }
+            UserModel userModel = new UserModel();
+            userModel.setName(name);
+            userModel.setLastName(lastName);
+            userModel.setPassword(password);
+            userModel.setLogin(login);
+            userRepo.save(userModel);
+        return true;
     }
 
     public String login(String login, String password){
