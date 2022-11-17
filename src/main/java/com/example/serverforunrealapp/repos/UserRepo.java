4318cb314584b43f71d3195d65f3a3ecd1134c21
Egor Ralovich <vonkad002@gmail.com>
2022-11-17 11:48:06 +0300

    Update UserRepo.java

--- src/main/java/com/example/serverforunrealapp/repos/UserRepo.java
+++ src/main/java/com/example/serverforunrealapp/repos/UserRepo.java
@@ -2,6 +2,8 @@ package com.example.serverforunrealapp.repos;
 
 import com.example.serverforunrealapp.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface UserRepo extends JpaRepository<UserModel, Long> {
     UserModel findUserModelByLogin(String login);
